600652f0bb902891bb7f77c258c11a1e8c7818a3
Daniel <dlf1987@gmail.com>
2023-04-28 14:46:28 -0400

    add mimic

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,9 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 
-export const InputControlBase = component$(() => {
-  return (
-    <>
-      <input class="border" autoComplete="postal-code" />
-    </>
-  );
-});
+export const InputControlBase = component$(
+  (props: { autoComplete: string }) => {
+    return (
+      <>
+        <input class="border" autoComplete={props.autoComplete} />
+      </>
+    );
+  }
+);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,17 +12,48 @@ export default component$(() => {
   return (
     <>
       <div>
+        <p>{global.count}</p>
+        <h3>Dashlane Extension</h3>
+        <p>reload page, if using Dashlane, click "click me"</p>
         <button
           style="margin-bottom: 10px"
           onClick$={() => {
             global.count = global.count + 1;
           }}
         >
-          click me! {global.count}
+          click me
         </button>
         <div>
           <span style="margin-right: 5px">(This will disappear)</span>
-          <InputControlBase />
+          <InputControlBase autoComplete="postal-code" />
+        </div>
+      </div>
+      <hr style="margin: 35px 0" />
+      <div>
+        <h3>Mimic Dashlane extension</h3>
+        <p>reload page, click "prepend element", then click "click me"</p>
+        <button
+          style="margin-bottom: 10px; margin-right: 10px"
+          onClick$={() => {
+            const container = document.getElementById('pcontainer')!;
+            const elem = document.createElement("span");
+            elem.innerHTML = '(mimic chrome extension)';
+            container.insertBefore(elem, container.firstChild);
+          }}
+        >
+          prepend element
+        </button>
+        <button
+          style="margin-bottom: 10px"
+          onClick$={() => {
+            global.count = global.count + 1;
+          }}
+        >
+          click me
+        </button>
+        <div id="pcontainer">
+          <span style="margin-right: 5px">(This will disappear)</span>
+          <InputControlBase autoComplete="off" />
         </div>
       </div>
     </>
